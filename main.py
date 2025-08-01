7f3ebc8d3bced69c083d4907a4a8a8793eb7124e
Viswazgummadi <viswazgummadi@gmail.com>
2025-08-01 11:45:39 +0530

    Initial commit to dev1

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import uvicorn
+import argparse
 from src.api.server import app
+import uvicorn
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=8000, help='Port to run the server on')
+    args = parser.parse_args()
+
+    uvicorn.run(app, host="0.0.0.0", port=args.port)
 
 if __name__ == "__main__":
-    # This block runs when you execute 'python main.py'
-    # It starts the Uvicorn server, telling it where to find our FastAPI app.
-    # host="0.0.0.0" makes it accessible on your local network.
-    # reload=True automatically restarts the server when you save a file,
-    # which is incredibly useful for development.
-    uvicorn.run("src.api.server:app", host="0.0.0.0", port=8000, reload=True)
+    main()
